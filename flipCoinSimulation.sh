a3f0c7425db5ab3be847577131fe3b9f8f60e13e
OjashviSingh <ojashvisingh365@gmail.com>
2022-12-31 03:36:47 +0530

    Extend UC 3 to ensure if its tie then the game continues till the difference of minimum 2 points is achieved.

--- flipCoinSimulation.sh
+++ flipCoinSimulation.sh
@@ -16,3 +16,16 @@ do
         echo "Tails"
         (( tails_count++ ))
 fi
+diff_bt_hc_tc=$(( heads_count - tails_count ))
+    if(( heads_count == target_count && diff_bt_hc_tc >= minimum_difference ))
+    then
+        echo "Heads won by $diff_bt_hc_tc points"
+        break
+    elif(( tails_count == target_count && ${diff_bt_hc_tc#-} >= minimum_difference ))
+    then
+         echo "Tails won by ${diff_bt_hc_tc#-} points"
+         break
+    fi
+    
+done
+echo "The Heads count is $heads_count and Tails Count is $tails_count"
